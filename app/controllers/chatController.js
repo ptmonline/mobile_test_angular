9ad4942e1c7a0e373d4a9e56abc82f579a33b32c
Pau Torres <ptmonlinework@gmail.com>
2016-03-31 14:26:36 +0200

    chat view

--- app/controllers/chatController.js
+++ app/controllers/chatController.js
@@ -1,13 +1,13 @@
 angular.module('MobileApp')
   .controller('chatController', function($scope){
-    var messagesArray = ['Hello there, how are you?', 'Today is a weird day', 'this is the third random message'];
+    var messagesArray = ['It has survived not only five centuries, but also the leap into electronic.', 'Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 'It is a long established fact that a reader will be distracted by the readable content.'];
     var randomMessage = Math.floor((Math.random() * 3));
     $scope.profileName = 'Sophie';
     $scope.message = messagesArray[randomMessage];
 
     //Check if there session stored;
     sessionStorage.getItem("stored_messages") !== null ? $scope.messages = JSON.parse(sessionStorage.getItem("stored_messages")) : $scope.messages=[];
-    
+
     //add message to messages array and stored to session storage
     $scope.addMessage = function(newmessage) {
       $scope.messages.push(newmessage);
